b7ffddec7fd8ef379fd737519ded8d845d156c85
WillIsback <william.derue@.com>
2025-11-13 23:55:42 +0000

    feat: ajouter la navigation dans le modal avec des boutons gauche et droite, améliorer le style du modal et des boutons

--- src/app/@modal/(.)photographer/[photographer]/photo/[photo]/modal.tsx
+++ src/app/@modal/(.)photographer/[photographer]/photo/[photo]/modal.tsx
@@ -1,13 +1,16 @@
 'use client';
 
 import { useEffect, useRef, useState } from 'react';
-import { useRouter } from 'next/navigation';
+import { useRouter, useParams } from 'next/navigation';
 import { createPortal } from 'react-dom';
+import Image from 'next/image';
 
 export function Modal({ children }: { children: React.ReactNode }) {
   const router = useRouter();
   const dialogRef = useRef<React.ComponentRef<'dialog'>>(null);
   const [mounted, setMounted] = useState(false);
+  const params = useParams<{ photographer: string; photo: string }>()
+
 
   useEffect(() => {
     setMounted(true);
@@ -19,14 +22,53 @@ export function Modal({ children }: { children: React.ReactNode }) {
   function onDismiss() {
     router.back();
   }
+  function goLeft() {
+    const newPhotoId = `${(Number(params.photo) - 1)}`;
+    router.push(`/photographer/${params.photographer}/photo/${newPhotoId}`);
+  }
+
+  function goRight() {
+    const newPhotoId = `${(Number(params.photo) + 1)}`;
+    router.push(`/photographer/${params.photographer}/photo/${newPhotoId}`);
+  }
 
   if (!mounted) return null;
+  console.log("useparam in modal = ",params)
 
   return createPortal(
     <div className="modal-backdrop">
       <dialog ref={dialogRef} className="modal" onClose={onDismiss}>
+        <button onClick={goLeft} className="modal__btn_nav">
+          <span>
+            <Image 
+              src='/modal_chevron_left.svg'
+              width={42}
+              height={42}
+              alt='button to close modal'
+            />
+          </span>
+        </button>
         {children}
-        <button onClick={onDismiss} className="close-button" />
+      <button onClick={goRight} className="modal__btn_nav">
+          <span>
+            <Image 
+              src='/modal_chevron_right.svg'
+              width={42}
+              height={42}
+              alt='button to close modal'
+            />
+          </span>
+        </button>
+        <button onClick={onDismiss} className="close-button">
+          <span>
+            <Image 
+              src='/close_ic.svg'
+              width={42}
+              height={42}
+              alt='button to close modal'
+            />
+          </span>
+        </button>
       </dialog>
     </div>,
     document.getElementById('modal-root')!
